89ec5a7226be2b98e1bc882ee8eec5ff9ebd64f8
basava2014 <nandigatti@gmail.com>
2014-12-11 12:29:17 +0530

    added before and after annotations

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/b0/c0f409ed4c8000141c159442e907cc59
@@ -0,0 +1,9 @@
+package testngpackage;
+
+import org.testng.annotations.Test;
+
+public class TestNGFile {
+  @Test
+  public void f() {
+  }
+}
--- TestNGProject/src/testngpackage/TestNGFile.java
+++ TestNGProject/src/testngpackage/TestNGFile.java
@@ -1,9 +1,29 @@
 package testngpackage;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestNGFile {
-  @Test
-  public void f() {
-  }
+	public String baseUrl = "http://facebook.com";
+	public WebDriver driver;
+  
+	@BeforeTest
+	public void setUrl() {
+	driver = new FirefoxDriver();
+	driver.get(baseUrl);
+	}
+	
+	@Test
+  public void VerifyFBPage() {
+		System.out.println("test passed");
+  }	
+		
+	@AfterTest
+	public void endSession() {
+	driver.quit();	
+	}
+	
 }
